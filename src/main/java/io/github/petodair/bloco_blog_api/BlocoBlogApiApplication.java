2e6781da72cb34689f7c004ee68049230e4c5e3a
Seu Nome <petersomodair@gmail.com>
2026-06-14 02:23:23 -0300

    commit incial

---
+++ src/main/java/io/github/petodair/bloco_blog_api/BlocoBlogApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.petodair.bloco_blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlocoBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlocoBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/petodair/bloco_blog_api/BlocoBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.petodair.bloco_blog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocoBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
